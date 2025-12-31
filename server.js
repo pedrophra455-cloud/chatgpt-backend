44322df31d35b665725ecaa1a61ff365a3b74236
pedrophra455-cloud <pedrophra455@gmail.com>
2025-12-31 07:57:22 -0400

    fix render port

--- server.js
+++ server.js
@@ -57,6 +57,8 @@ app.post("/audio", upload.single("audio"), async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
